ada31c76c71dc15392a122f1b46a6c1c3eaf0468
yzq <504767693@qq.com>
2021-03-31 14:30:34 +0800

    初始化环境

---
+++ src/vuex/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = {
+  state: {
+
+  },
+  
+  mutations: {
+
+  },
+
+  actions: {
+
+  },
+
+  modules: {
+
+  }
+}
+
+const vueStore = new Vuex.Store(store)
+
+export default vueStore
