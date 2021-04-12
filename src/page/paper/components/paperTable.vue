f23d6b8114313ac123649e9ce0757eaf1dfc1d87
yzq <504767693@qq.com>
2021-04-12 18:18:29 +0800

    paper列表展示

--- src/page/paper/components/paperTable.vue
+++ src/page/paper/components/paperTable.vue
@@ -8,7 +8,21 @@
       <el-table-column label="考试年份" prop="examYear" align="center"></el-table-column>
       <el-table-column label="考试形式" prop="examFomat.title" align="center"></el-table-column>
       <el-table-column label="试卷状态" prop="paperStatus.title" align="center"></el-table-column>
-      <el-table-column label="套题详情" prop="paperStatus.title" align="center"></el-table-column>
+      <el-table-column label="套题详情" align="center" type="expand">
+        <template slot-scope="scope">
+          <el-table :data="scope.row.questionList">
+            <el-table-column label="ID" prop="id" align="center"></el-table-column>
+            <el-table-column label="题序" prop="sorting" align="center"></el-table-column>
+            <el-table-column label="题干" prop="content" align="center" width="500px">
+              <template slot-scope="scope">
+                <span v-html="scope.row.content"></span>
+              </template>
+            </el-table-column>
+            <el-table-column label="操作" align="center">
+            </el-table-column>
+          </el-table>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
